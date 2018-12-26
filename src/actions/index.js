35fbb755ec90f2dcf397d6c7923267235a2f9d7b
Hessam <hessam199700@gmail.com>
2018-12-26 16:35:19 +0330

    making codes a bit clean for actions Index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,125 +1,81 @@
-import axios from 'axios';
+import axios from "axios";
 
 //const URL = `http://chetor.com/wp-json/wp/v2/posts?_embed&page=1`;
 
-export function articlesListAll(URL){
-    const request = axios.get(`${URL}`)
-                    .then(response => response.data)
-     
-    return {
-        type: 'GET_ARTICLES_ALL',
-        payload: request 
-        
-    };
+export function articlesListAll(URL) {
+  const request = axios.get(`${URL}`).then(response => response.data);
+
+  return {
+    type: "GET_ARTICLES_ALL",
+    payload: request
+  };
 }
 
-export function articlesListMore(URL){
-    const request = axios.get(`${URL}`)
-                    .then(response => response.data)
-      
-    return {
-        type: 'GET_ARTICLES_MORE',
-        payload:request //{request , currentURL : URL}
-    };
+export function articlesListMore(URL) {
+  const request = axios.get(`${URL}`).then(response => response.data);
+
+  return {
+    type: "GET_ARTICLES_MORE",
+    payload: request
+  };
 }
 
 export function latestURL(URL) {
-    return {
-        type : 'LATEST_URL' ,
-        payload : URL 
-    }
+  return {
+    type: "LATEST_URL",
+    payload: URL
+  };
 }
 
 export function loadFalser() {
-    return {
-        type : 'LOAD_FALSER' ,
-        payload : null
-    }
+  return {
+    type: "LOAD_FALSER",
+    payload: null
+  };
 }
 
 export function loadMoreFalser() {
-    return {
-        type : 'LOAD_MORE_FALSER' ,
-        payload : null
-    }
+  return {
+    type: "LOAD_MORE_FALSER",
+    payload: null
+  };
 }
 
 export function incrementLoadedPage(pageNum) {
-    return {
-        type : 'INCREMENT_LOADED_PAGE' ,
-        payload : pageNum+1
-    }
+  return {
+    type: "INCREMENT_LOADED_PAGE",
+    payload: pageNum + 1
+  };
 }
 
 export function getSingleArticle(URL) {
-    const request = axios.get(`${URL}`)
-                    .then(response => response.data)
+  const request = axios.get(`${URL}`).then(response => response.data);
 
-    return {
-        type : 'GET_SINGLE_ARTICLE' ,
-        payload : request
-    }
+  return {
+    type: "GET_SINGLE_ARTICLE",
+    payload: request
+  };
 }
 
 export function singleLoadFalser() {
-    return {
-        type : 'SINGLE_LOAD_FALSER' ,
-        payload : null
-    }
+  return {
+    type: "SINGLE_LOAD_FALSER",
+    payload: null
+  };
 }
 
-export function headerTitle (headerName){
-
-    return {
-        type : 'HEADER_TITLE' ,
-        payload : headerName 
-    }
+export function headerTitle(headerName) {
+  return {
+    type: "HEADER_TITLE",
+    payload: headerName
+  };
 }
 
-export function loadFontTruth () {
-    const fontStatus = {fontIsLoaded : true } 
+export function loadFontTruth() {
+  const fontStatus = { fontIsLoaded: true };
 
-    return {
-        type : 'LOAD-FONT' ,
-        payload : fontStatus 
-    }
+  return {
+    type: "LOAD-FONT",
+    payload: fontStatus
+  };
 }
-
-
-
-
-
-
-
-
-
-/*
-export function selectedArticle(id){
-    return {
-        type : 'SELECTED_ARTICLE' ,
-        payload : id 
-    };
-}
-*/
-
-/*
-        console.log("object Mounted !!!");
-        axios.get('http://chetor.com/wp-json/wp/v2/posts?_embed&page=1')
-            .then(Response => this.setState({cards : Response.data})) ;
-            componentDidMount = () => {
-                fetch('http://chetor.com/wp-json/wp/v2/posts?_embed&page=1', {
-                    method: 'GET'
-                })
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    console.log(responseJson);
-                    this.setState({
-                        cards: responseJson
-                    })
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-                }
-                //Alert.alert(this.state.albums.length === 0 ? 'zero':'more');
-                */
