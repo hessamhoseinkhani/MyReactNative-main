0ad5c28c2531d11a2cb558aa2c1c9c47406a6b4a
Hessam <hessam199700@gmail.com>
2018-12-26 15:39:09 +0330

    making codes a bit clean for App , Index and Home

--- App.js
+++ App.js
@@ -1,139 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View , ScrollView , Button , Alert} from 'react-native';
-import { createStackNavigator, createAppContainer , createDrawerNavigator} from "react-navigation";
-import { Font } from 'expo';
-import {Drawer} from 'native-base';
+// requiered react native libraries
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Alert
+} from "react-native";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createDrawerNavigator
+} from "react-navigation";
+import { Font } from "expo";
+import { Drawer } from "native-base";
 
-import Header from './src/components/header';
-import EachCard from './src/components/eachCard';
-import Cards from './src/components/cards';
-import ArticleComponent from './src/components/articleContent'
-import Home from './src/components/Home';
-import Index from './src/components';
-import CategoriesComponent from './src/components/categoriesComponent'
-//Redux requiered libraries
-import { Provider } from 'react-redux';
-import { createStore , applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import reducers from './src/reducers';
+// required Redux requiered libraries
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
 
+// requierd Components
+import Index from "./src/components";
+import Home from "./src/components/Home";
+import Cards from "./src/components/cards";
+import ArticleComponent from "./src/components/articleContent";
+import CategoriesComponent from "./src/components/categoriesComponent";
+import EachCard from "./src/components/eachCard";
+import Header from "./src/components/header";
+import reducers from "./src/reducers";
 
+const createStoreWithMiddlewre = applyMiddleware(promiseMiddleware)(
+  createStore
+);
 
-const createStoreWithMiddlewre = applyMiddleware(promiseMiddleware)(createStore);
-
- class App extends React.Component {
+class App extends React.Component {
   static navigationOptions = {
-    header : null
+    header: null
   };
 
-
-
   render() {
-    console.log('@@@@ we are in app.js @@@');
-    //console.log(this.props);
-    
+    //console.log('@@@@ we are in app.js @@@' , this.props);
+
     return (
-        
-        <Provider store={createStoreWithMiddlewre(reducers)}>
-          <Index  {...this.props} /> 
-        </Provider>
-              
-
-      
-
+      <Provider store={createStoreWithMiddlewre(reducers)}>
+        <Index {...this.props} />
+      </Provider>
     );
   }
 }
-// index => Home
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonStyle: {
-    marginLeft: 125 ,
-    marginRight : 125
-  } , 
-  appBar : {
-    backgroundColor : "black"
-  } ,
-  textStyle : {
-    marginLeft: 16 ,
-  } ,
-  iconsStyle : {
-    color : "grey"
-  } ,
-  cardStyle : {
-    width: 100, 
-    height: 100 ,
-    alignSelf :'flex-end' ,
-    marginRight : 25 ,
-    flexDirection: 'row',
-  }, 
-  rowStyle : {
-    flexDirection: 'row'
-  } ,
-  paragraghStyle : {
-    width: 200, 
-    height: 70 , 
-    marginTop : 10 ,
-    marginRight : 25 ,
-    fontSize : 20 , 
-    fontWeight : 'bold'
-  } , 
-  titleStyle : {
-    fontSize : 15 ,
-    fontFamily: "normal" , 
-    color : 'grey' ,
-  } ,
-  nonTitleStyle : {
-    fontSize : 15 ,
-    fontFamily: "normal" ,
-  } ,
-  
-});
-//just changed createStackNavigator to createDrawerNavigator
-// const AppNavigator = createStackNavigator({
-//   Home: { screen: App } ,
-//   Article : { screen : ArticleComponent} ,
-// });
 
-// export default createAppContainer(AppNavigator);
-
-// const AppDrawerNavigator =  createDrawerNavigator({
-//   Home : {screen : Index} ,  
-//   Categories : CategoriesComponent //{screen :  (props) => <CategoriesComponent {...props} /> } 
-// });
-
-
-// const AppContainer = createAppContainer(AppDrawerNavigator);
-
-
-// export default AppContainer;
-
- export default App ;
-
-// const AppDrawerNavigator =  createDrawerNavigator({
-//   Home : {screen : App} ,  
-//   Categories :({ navigation }) => <CategoriesComponent {...this.props}  store={createStoreWithMiddlewre(reducers)}/> 
-// });
-
-// const AppNavigator = createStackNavigator({
-//   Drawer : {screen : AppDrawerNavigator},
-//   Home: { screen: App } ,
-//   Article : { screen : ArticleComponent} ,
-// }, 
-// {
-//   headerMode: 'none',
-//   navigationOptions: {
-//     headerVisible: false,
-//   }
-//  }
-// );
-
-
-
-// export default createAppContainer(AppNavigator);
-
+export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,191 +1,161 @@
-import React from 'react';
-import { StyleSheet, Text, View , ScrollView , Alert , Animated} from 'react-native';
-import { Font } from 'expo';
-// Redux
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// requiered react native libraries
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Alert,
+  Animated
+} from "react-native";
+import { Font } from "expo";
+// required Redux requiered libraries
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-//importing files
+// requierd Components
+import Header from "./header";
+import EachCard from "./eachCard";
+import reducers from "../reducers";
+import CardsContainer from "../containers/cards_container";
+import {
+  articlesListMore,
+  latestURL,
+  incrementLoadedPage,
+  loadMoreFalser,
+  headerTitle
+} from "../actions";
 
-import Header from './header';
-import EachCard from './eachCard';
-import Cards from './cards';
-import reducers from '../reducers';
-import CardsContainer from '../containers/cards_container';
-
-import { articlesListMore , latestURL , incrementLoadedPage , loadMoreFalser , headerTitle} from '../actions';
-//import { loadFontTruth } from '../actions';
-//import { Icon } from 'react-native-paper/typings/components/List';
-import { Icon } from 'native-base';
-
-
-const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
   const paddingToBottom = 20;
-  return layoutMeasurement.height + contentOffset.y >=
-    contentSize.height - paddingToBottom;
+  return (
+    layoutMeasurement.height + contentOffset.y >=
+    contentSize.height - paddingToBottom
+  );
 };
-
-
 
 class Home extends React.Component {
-
   state = {
     fontLoaded: false,
-    viewedPage : 2
+    viewedPage: 2
   };
 
-
-
-
-
   async componentDidMount() {
-    
     await Font.loadAsync({
-      'IRANYekanMobile-Regular': require('../../assets/fonts/IRANYekanMobile-Regular.ttf'),
-      'IRANYekanMobile-Bold': require('../../assets/fonts/IRANYekanMobile-Bold.ttf'),
-      'IRANYekanMobile-Light': require('../../assets/fonts/IRANYekanMobile-Light.ttf'),
+      "IRANYekanMobile-Regular": require("../../assets/fonts/IRANYekanMobile-Regular.ttf"),
+      "IRANYekanMobile-Bold": require("../../assets/fonts/IRANYekanMobile-Bold.ttf"),
+      "IRANYekanMobile-Light": require("../../assets/fonts/IRANYekanMobile-Light.ttf")
     });
     this.setState({ fontLoaded: true });
-
-  
-
   }
-  
 
-/*
-
-const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-  const paddingToBottom = 20;
-  return layoutMeasurement.height + contentOffset.y >=
-    contentSize.height - paddingToBottom;
-};
-
-//const MyCoolScrollViewComponent = ({enableSomeButton}) => (
-  
-  <ScrollView
-    onScroll={({nativeEvent}) => {
-      if (isCloseToBottom(nativeEvent)) {
-        Alert.alert("asdad");
-      }
-    }}
-    scrollEventThrottle={400}
-  >
-
-*/
-    static navigationOptions = {
-      drawerIcon : ({ tintColor }) => (
-        <Icon name="home" style={{ fontSize : 24 , color :  "#0090B0"}}/> 
-      )
-    }
-
-   
+  static navigationOptions = {
+    drawerIcon: ({ tintColor }) => (
+      <Icon name="home" style={{ fontSize: 24, color: "#0090B0" }} />
+    )
+  };
 
   render() {
-    
-   // console.log("data i am seeking for");this.props.headerTitle('')
-    //console.log(this.props);
-    //console.log("data is loaded :" , this.props.data.currentURL);
+    //console.log("data i am seeking for" , this.props);
+
     return (
-      
-      <View> 
-        {    
-          this.state.fontLoaded ? (
-
-            <View>
-              
+      <View>
+        {this.state.fontLoaded ? (
+          <View>
             <Header {...this.props} />
-           
-            <Animated.ScrollView 
-            removeClippedSubviews={true}
-            style={{marginBottom:155}}     
-            onScroll={({nativeEvent}) => {
-              if (isCloseToBottom(nativeEvent)) {
-                //this.setState({ viewedPage : this.state.viewedPage+1});
-                this.props.loadMoreFalser();
-                this.props.incrementLoadedPage(this.props.data.LoadedPage);
-                this.props.articlesListMore(this.props.data.currentURL+this.props.data.LoadedPage);
-                //this.props.latestURL(`http://chetor.com/wp-json/wp/v2/posts?_embed&page=`);
-                //Alert.alert("asdad");
-              }
-            }}
-            scrollEventThrottle={400}>
-              
-              <CardsContainer {...this.props}/>
+
+            <Animated.ScrollView
+              removeClippedSubviews={true}
+              style={{ marginBottom: 155 }}
+              onScroll={({ nativeEvent }) => {
+                if (isCloseToBottom(nativeEvent)) {
+                  this.props.loadMoreFalser();
+                  this.props.incrementLoadedPage(this.props.data.LoadedPage);
+                  this.props.articlesListMore(
+                    this.props.data.currentURL + this.props.data.LoadedPage
+                  );
+                }
+              }}
+              scrollEventThrottle={400}
+            >
+              <CardsContainer {...this.props} />
             </Animated.ScrollView>
-
-            </View>
-
-          ) : null
-        
-
-        }
+          </View>
+        ) : null}
       </View>
     );
   }
 }
-// <Cards {...this.props}/>
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   buttonStyle: {
-    marginLeft: 125 ,
-    marginRight : 125
-  } , 
-  appBar : {
-    backgroundColor : "black"
-  } ,
-  textStyle : {
-    marginLeft: 16 ,
-  } ,
-  iconsStyle : {
-    color : "grey"
-  } ,
-  cardStyle : {
-    width: 100, 
-    height: 100 ,
-    alignSelf :'flex-end' ,
-    marginRight : 25 ,
-    flexDirection: 'row',
-  }, 
-  rowStyle : {
-    flexDirection: 'row'
-  } ,
-  paragraghStyle : {
-    width: 200, 
-    height: 70 , 
-    marginTop : 10 ,
-    marginRight : 25 ,
-    fontSize : 20 , 
-    fontWeight : 'bold'
-  } , 
-  titleStyle : {
-    fontSize : 15 ,
-    fontFamily: "normal" , 
-    color : 'grey' ,
-  } ,
-  nonTitleStyle : {
-    fontSize : 15 ,
-    fontFamily: "normal" ,
-  } ,
-  
+    marginLeft: 125,
+    marginRight: 125
+  },
+  appBar: {
+    backgroundColor: "black"
+  },
+  textStyle: {
+    marginLeft: 16
+  },
+  iconsStyle: {
+    color: "grey"
+  },
+  cardStyle: {
+    width: 100,
+    height: 100,
+    alignSelf: "flex-end",
+    marginRight: 25,
+    flexDirection: "row"
+  },
+  rowStyle: {
+    flexDirection: "row"
+  },
+  paragraghStyle: {
+    width: 200,
+    height: 70,
+    marginTop: 10,
+    marginRight: 25,
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  titleStyle: {
+    fontSize: 15,
+    fontFamily: "normal",
+    color: "grey"
+  },
+  nonTitleStyle: {
+    fontSize: 15,
+    fontFamily: "normal"
+  }
 });
 
-
-
-function mapStateToProps(state){
-  return {      
-    data : state.articles
-  }
+function mapStateToProps(state) {
+  return {
+    data: state.articles
+  };
 }
 
-
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({articlesListMore  , latestURL , incrementLoadedPage , loadMoreFalser , headerTitle} , dispatch)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      articlesListMore,
+      latestURL,
+      incrementLoadedPage,
+      loadMoreFalser,
+      headerTitle
+    },
+    dispatch
+  );
 }
 
-export default connect(mapStateToProps , mapDispatchToProps)(Home);
-//export default Home;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
