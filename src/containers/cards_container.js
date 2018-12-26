1bce1518dede04c4aa8534525c2bea2e36d8361a
Hessam <hessam199700@gmail.com>
2018-12-26 16:12:12 +0330

    making codes a bit clean for Cards_Container

--- src/containers/cards_container.js
+++ src/containers/cards_container.js
@@ -1,66 +1,54 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Button , Card , Title, Paragraph ,Drawer ,Appbar ,DefaultTheme } from 'react-native-paper';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-// Importing Components and Files
-import EachCard from '../components/eachCard';
-import ArticlesList from '../components/articlesList'
-import { articlesListAll , latestURL } from '../actions';
+// requiered react native libraries
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  Drawer,
+  Appbar,
+  DefaultTheme
+} from "react-native-paper";
 
+// required Redux requiered libraries
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-
-
-
+// requierd Components
+import EachCard from "../components/eachCard";
+import ArticlesList from "../components/articlesList";
+import { articlesListAll, latestURL } from "../actions";
 
 class CardsContainer extends Component {
-/*
-    state = {
-        cards : []
-    };
-*/
-    componentWillMount(){
-        this.props.articlesListAll(`http://chetor.com/wp-json/wp/v2/posts?_embed&page=1`);
-        this.props.latestURL(`http://chetor.com/wp-json/wp/v2/posts?_embed&page=`);
-    }
+  componentWillMount() {
+    this.props.articlesListAll(
+      `http://chetor.com/wp-json/wp/v2/posts?_embed&page=1`
+    );
+    this.props.latestURL(`http://chetor.com/wp-json/wp/v2/posts?_embed&page=`);
+  }
 
-    render() { 
-
-        // console.log("****** we are in cards container");
-        return (
-            <View>
-                <ArticlesList {...this.props} />
-            </View>
-        );
-    }
-
-    getImgUrl = (str) => {
-        if (str !== ''){
-            let arr=[];
-            arr[0] = str.slice(str.search(/src="/i));
-            arr[1] = arr[0].slice(arr[0].search(/"/i)+1);
-            arr[0] = arr[1].slice(0 , arr[1].search(/"/i));
-            return arr[0];
-        }
-        else 
-            return "https://fakeimg.pl/300/";
-    }
-}
- 
-
-function mapStateToProps(state){
-    return {
-        data : state.articles
-    }
+  render() {
+    // console.log("****** we are in cards container");
+    return (
+      <View>
+        <ArticlesList {...this.props} />
+      </View>
+    );
+  }
 }
 
-
-function mapDispatchToProps(dispatch){
-    return bindActionCreators({articlesListAll , latestURL} , dispatch)
+function mapStateToProps(state) {
+  return {
+    data: state.articles
+  };
 }
-//export default Cards;
-export default connect(mapStateToProps , mapDispatchToProps)(CardsContainer);
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ articlesListAll, latestURL }, dispatch);
+}
 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CardsContainer);
