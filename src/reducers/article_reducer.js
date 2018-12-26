046580e0e6f25da13d104e5ffd01ce49b188072e
Hessam <hessam199700@gmail.com>
2018-12-26 16:33:41 +0330

    making codes a bit clean for articles_reducer

--- src/reducers/article_reducer.js
+++ src/reducers/article_reducer.js
@@ -1,33 +1,37 @@
-export default function(state = {} , action){
-    switch(action.type){
-        case 'LOAD_FALSER'  :
-            return  {...state , Loaded : false  }
-        
-        case 'LOAD_MORE_FALSER'  :
-            return  {...state , MoreLoaded : true  }
+export default function(state = {}, action) {
+  switch (action.type) {
+    case "LOAD_FALSER":
+      return { ...state, Loaded: false };
 
-        case 'SINGLE_LOAD_FALSER'  :
-            return  {...state , SingleLoaded : false  }
+    case "LOAD_MORE_FALSER":
+      return { ...state, MoreLoaded: true };
 
-        case    'HEADER_TITLE'  :
-            return  {...state , HeaderTitle : action.payload  } 
+    case "SINGLE_LOAD_FALSER":
+      return { ...state, SingleLoaded: false };
 
-        case 'INCREMENT_LOADED_PAGE'  :
-            return  {...state , LoadedPage : action.payload  }
+    case "HEADER_TITLE":
+      return { ...state, HeaderTitle: action.payload };
 
-        case 'GET_ARTICLES_ALL':
-            return {...state , articles:action.payload , Loaded : true  }
-        
-        case 'GET_SINGLE_ARTICLE':
-            return {...state , singleArticle : action.payload , SingleLoaded : true  }
+    case "INCREMENT_LOADED_PAGE":
+      return { ...state, LoadedPage: action.payload };
 
-        case 'GET_ARTICLES_MORE' :
-            return {...state , articles : [ ...state.articles , ...action.payload] , MoreLoaded : false  }
+    case "GET_ARTICLES_ALL":
+      return { ...state, articles: action.payload, Loaded: true };
 
-        case 'LATEST_URL' : 
-            return {...state , currentURL : action.payload , LoadedPage : 2}
-        default:
-            return state;
-    }
+    case "GET_SINGLE_ARTICLE":
+      return { ...state, singleArticle: action.payload, SingleLoaded: true };
+
+    case "GET_ARTICLES_MORE":
+      return {
+        ...state,
+        articles: [...state.articles, ...action.payload],
+        MoreLoaded: false
+      };
+
+    case "LATEST_URL":
+      return { ...state, currentURL: action.payload, LoadedPage: 2 };
+    default:
+      return state;
+  }
 }
-//, currentURL : action.payload.currentURL   
+//, currentURL : action.payload.currentURL
